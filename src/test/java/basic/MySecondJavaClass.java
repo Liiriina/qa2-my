3e4725c86a37f9f3bbf29b3fec565425a441ffd7
Irina <liiriina@gmail.com>
2021-10-27 21:08:29 +0300

    Initial commit

---
+++ src/test/java/basic/MySecondJavaClass.java
@@ -0,0 +1,10 @@
+package basic;
+
+import basic.MyFirstJavaClass;
+
+public class MySecondJavaClass {
+    private void test() {
+        MyFirstJavaClass firstClass = new MyFirstJavaClass();
+        firstClass.sumTwoDigits(2, 5);
+    }
+}
