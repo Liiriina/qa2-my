ad27be25033c0d8b98bd06afd565b7a622a9414c
Irina <liiriina@gmail.com>
2021-11-14 14:16:40 +0200

    commit

--- src/test/java/selenium/JanisRoze.java
+++ src/test/java/selenium/JanisRoze.java
@@ -13,7 +13,6 @@ public class JanisRoze {
 
     private final String HOME_PAGE_URL = "https://www.janisroze.lv/";
     private final By ACCEPT_COOKIES_BTN = By.xpath(".//button[contains(@class,'ch2-btn-text-xxs')]");
-    private final By PROFILE_HEADER = By.xpath(".//[@id='header-account']");
 
     private WebDriver browser;
 
@@ -26,7 +25,6 @@ public class JanisRoze {
         browser.get(HOME_PAGE_URL);
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
         browser.findElement(ACCEPT_COOKIES_BTN).click();
-        browser.findElement(PROFILE_HEADER).click();
 
 
 
