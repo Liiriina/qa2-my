56c162abb316abc6a94bc43dfb6f1364ad63c69b
Irina <liiriina@gmail.com>
2021-11-14 15:04:59 +0200

    commit

--- src/test/java/selenium/JanisRoze.java
+++ src/test/java/selenium/JanisRoze.java
@@ -1,5 +1,6 @@
 package selenium;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,6 +14,10 @@ public class JanisRoze {
 
     private final String HOME_PAGE_URL = "https://www.janisroze.lv/";
     private final By ACCEPT_COOKIES_BTN = By.xpath(".//button[contains(@class,'ch2-btn-text-xxs')]");
+    private final By DROPDOWN_MENU = By.xpath(".//ul[contains(@class,'account-dropdown long')]");
+    private final By LOGIN_INPUT = By.id("email");
+    private final By PASSWORD_INPUT = By.id("pass");
+    private final By LOGIN_BTN = By.id("send2");
 
     private WebDriver browser;
 
@@ -25,9 +30,17 @@ public class JanisRoze {
         browser.get(HOME_PAGE_URL);
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
         browser.findElement(ACCEPT_COOKIES_BTN).click();
-
+        browser.findElement(DROPDOWN_MENU).click();
+        browser.findElement(LOGIN_INPUT).sendKeys("annakalinina3472@gmail.com");
+        browser.findElement(PASSWORD_INPUT).sendKeys("Lovemyjob123");
+        browser.findElement(LOGIN_BTN).click();
 
 
     }
 
+    @AfterEach
+    public void closeBrowser() {
+        browser.close();
+
+    }
 }
